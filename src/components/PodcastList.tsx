d3df9125b8f5122e2f54e2f86a2537a3b902ea24
Matt Batman <batman.matt@gmail.com>
2024-01-22 18:49:29 -0500

    Keep margin between list and filters

--- src/components/PodcastList.tsx
+++ src/components/PodcastList.tsx
@@ -5,7 +5,7 @@ import { Podcast } from '../types';
 
 function PodcastList({ podcasts }: { podcasts: Podcast[] }) {
   return (
-    <ul>
+    <ul className="PodcastList">
       {podcasts.map((podcast: Podcast) => (
         <PodcastCard
           podcast={podcast}
